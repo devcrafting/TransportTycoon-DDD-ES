2b72136093f477356b44e7c746063f9a7dd83fd9
Clément Bouillier <clement.bouillier@devcrafting.com>
2019-11-21 00:10:45 +0100

    Add cmd line support

--- Program.fs
+++ Program.fs
@@ -1,5 +1,20 @@
 ﻿open Expecto
+open TransportTycoon.Domain
+open TransportTycoon.Infra
 
 [<EntryPoint>]
 let main argv =
-    runTestsInAssembly defaultConfig argv
+    if argv.Length = 1 then
+        let request =
+            argv.[0]
+            |> Seq.map (function
+                | 'A' -> WarehouseA
+                | 'B' -> WarehouseB)
+            |> Seq.toList
+        let finalState = 
+            computeHowLongItTakesToDeliver request
+                [ Truck 0 WaitingAt Factory; Truck 1 WaitingAt Factory; Boat 0 WaitingAt Port ]
+        (snd finalState).History |> writeLogs
+        0
+    else
+        runTestsInAssembly defaultConfig argv
